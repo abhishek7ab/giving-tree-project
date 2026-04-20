9b66945febeb61a46060cd73f30e2801f49f1315
My_Demo <badaveabhishek2004@gmail.com>
2026-04-20 21:49:37 +0530

    fix: frontend API + cloudinary images

--- database/db.js
+++ database/db.js
@@ -4,10 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  },
-  connectionTimeoutMillis: 10000,
-  idleTimeoutMillis: 30000,
-  max: 10
+  }
 });
 
 module.exports = pool;
