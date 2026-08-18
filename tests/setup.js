16346e6d370e619b265b6247b905d897b922f007
Abhishek Badave <badaveabhishek2004@gmail.com>
2026-08-18 19:11:58 +0530

    chore: remove tests folder

--- tests/setup.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: '.env.test' });
-
-process.env.NODE_ENV = 'test';
-process.env.JWT_SECRET = 'test-secret-key-that-is-at-least-32-chars-long';
-process.env.DATABASE_URL = 'postgresql://test:test@localhost:5432/test';
-process.env.FRONTEND_URL = 'http://localhost:3000';
-
-jest.setTimeout(10000);
